cb02caf6b10463556c1d2081586f435d4b9d07e5
mohsen <mohsen.abdalla.98@gmail.com>
2020-06-05 09:54:27 +0300

    serch recipes works (need to return a res acoording to API)

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,16 @@ var router = express.Router();
 const DButils = require("./utils/DButils");
 const bcrypt = require("bcrypt");
 
-// ------------------- Authentication - using cookie ------------------
+// ------------------- Authentication - middleware - using cookie ------------------
 router.use((req, res, next) =>{
   if(req.session && req.session.user_id){
     const id = req.session.user_id;
     const user =  DButils.checkUserNameOnDb(id); 
     if(user){
       req.user = user;
-      return next(); // if true go to the next relative endpoint
+      next(); // if true go to the next relative endpoint
     }
-  }
-  res.sendStatus(401);
+  }else res.sendStatus(401);
 });
 
 //--------------------- EndPoints ----------------------
@@ -22,10 +21,13 @@ router.get("/alive", (req, res) => {
   res.send("I'm alive")
 });
 
-router.get("/user/recipeInfo/{ids}", (req, res) => {
-  const ids = req.params.ids;
+router.get("/recipeInfo/:ids", (req, res) => {
+  //using JSON parse to get array of integers instead of array of strings
+  const ids = JSON.parse(req.params.ids);
   const user_name = req.user;
-  getUserInfoOnRecipes(user_name, ids); // TODO: build function 
+  console.log(ids, user_name);
+  const userRecipesData = getUserInfoOnRecipes(user_name, ids); //TODO: build function - go to DB and get foreach id if the user (watched, saved) 
+  res.send(userRecipesData);
 });
 
 module.exports = router;
--- routes/utils/search_recipes.js
+++ routes/utils/search_recipes.js
@@ -4,11 +4,11 @@ var express = require("express");
 var router = express.Router();
 
 const recipes_api_url = "https://api.spoonacular.com/recipes";
-const api_key = "apiKey=aa98be12b3104711bff1dcc28d9e4af0";
+const api_key = "apiKey=aa98be12b3104711bff1dcc28d9e4af0"; // kind of secret - usualy need to be in external file
 
 function extractQueriesParams(query_params, search_params){
     const param_list = ["diet", "cuisine", "imtolerance"];
-
+    //we change the original object that we got as a parameter...
     param_list.forEach((param) => {
         if(query_params[param]){
             search_params[param] = query_params[param];
@@ -18,6 +18,7 @@ function extractQueriesParams(query_params, search_params){
     console.log(search_params);
 }
 
+//first step of searching
 async function searchRecipes(search_params){
     let search_response = await axios.get(
         `${recipes_api_url}/search?${api_key}`,
@@ -25,7 +26,7 @@ async function searchRecipes(search_params){
             params: search_params,
         }
     );
-
+    //search response
     const recipes_id_list = extractSearchResultsIds(search_response);
     console.log(recipes_id_list);
 
@@ -38,7 +39,7 @@ async function searchRecipes(search_params){
 
 async function getRecipesInfo(recipes_id_list){
     let promises = [];
-
+    //insert to the promise array the info of the recipes by id
     recipes_id_list.map((id) => 
         promises.push(axios.get(`${recipes_api_url}/${id}/information?${api_key}`))
     );
@@ -50,6 +51,30 @@ async function getRecipesInfo(recipes_id_list){
     return relevantRecipeData;
 }
 
+function extractRelevantRecipeData(recipes_Info){
+    return recipes_Info.map((recipes_Info) =>{
+        const {
+            id,
+            title,
+            readyInMinutes,
+            aggregateLikes,
+            vegetarian,
+            vegan,
+            glutenFree,
+            image,
+        } = recipes_Info.data;
+        return{
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            aggregateLikes: aggregateLikes,
+            vegetarian: vegetarian,
+            vegan: vegan,
+            glutenFree: glutenFree,
+            image: image,
+        }
+    });
+}
 async function promiseAll(func, param_list){
     let promises = [];
     param_list.map((param) => promises.push(func(param)));
@@ -58,6 +83,7 @@ async function promiseAll(func, param_list){
     return info_responce;
 }
 
+//get the id of the recipes
 function extractSearchResultsIds(search_response){
     recipes_id_list = [];
     let recipes = search_response.data.results;
